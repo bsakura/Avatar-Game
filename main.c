0f2162cb69858bb3b75ecb17ccdcd76341917cf9
arfaghif <arfasen17@gmail.com>
2019-10-25 10:22:53 +0700

    nambahin

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    char s[1212];
+    scanf("%s", &s);
+    int i=0;
+    while (s[i]!='\0'){
+        printf("%c",s[i]);
+        i++;
+    }    
+    printf("\n");
+    return 0;
+}
